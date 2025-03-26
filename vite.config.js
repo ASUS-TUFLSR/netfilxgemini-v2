94ed307c2fec90ccec4327ff3275678bd0e315eb
Manmit Pal <manmitpal348@gmail.com>
2025-03-26 15:00:31 +0530

    error in tailwind resolved while deploying

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import path from "path";
+import "tailwind-scrollbar-hide";
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),require("tailwind-scrollbar-hide")],
+  plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       "/api": "http://localhost:5001",
